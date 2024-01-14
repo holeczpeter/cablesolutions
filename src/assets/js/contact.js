e2bdaa06c1fb30c961e64c25bdc7bcd6abb6623b
holeczpeter <44135203+holeczpeter@users.noreply.github.com>
2024-01-14 20:00:49 +0000

    Build: (3b98929) modify publish 3

--- src/assets/js/contact.js
+++
@@ -1,5 +0,0 @@
-export const contact = {
-    email: "administration@cablesolutions.eu",
-    phone: "(+31) 6 21955470",
-    address: "Cable Solutions Kft. Dózsa Telep 83. 6060 Tiszakécske"
-  };
--- src/assets/js/social.js
+++
@@ -1,5 +0,0 @@
-export const socialmedia = {
-    facebook: "https://www.facebook.com/profile.php?id=100064279564055&sk=about",
-    linkedIn: "https://www.linkedin.com/company/cable-solutions-kft-/about/"
-  };
-
