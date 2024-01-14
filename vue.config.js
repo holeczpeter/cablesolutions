e8a5f083735239b72c2f80ce41f50da65c88f25c
holeczpeter <holecz.peter85@gmail.com>
2024-01-14 21:10:48 +0100

    fix config

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: './',
-  outputDir:'dist',
-  assetsDir:'static',
+  publicPath: process.env.NODE_ENV === "production" ? "/" : "/",
   pluginOptions: {
     i18n: {
       locale: 'hu',
