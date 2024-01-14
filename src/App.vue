929b666cb0123020d0f6e473d09de5b5ab69e4dc
holeczpeter <holecz.peter85@gmail.com>
2024-01-14 21:42:17 +0100

    fix app template

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,11 @@
 <template>
-<html>
-<head>
-</head>
-
-<body>
-<div id="app" class="page">
+ <div id="app" class="page">
   <router-view></router-view>
   <Footer/>
 </div>
-</body>
-</html>
-
-
 </template>
 
 <script>
-
 import Footer from './components/Footer.vue'
 export default {
   name: 'App',
