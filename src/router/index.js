99d8959054ba6738ba60dcee1b3c87cc18ebce4e
holeczpeter <holecz.peter85@gmail.com>
2024-01-17 10:04:27 +0100

    off history

--- src/router/index.js
+++ src/router/index.js
@@ -35,7 +35,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  //history: createWebHistory(process.env.BASE_URL),
   routes,
   scrollBehavior(to, from, savedPosition) {
     if (to.hash) {
