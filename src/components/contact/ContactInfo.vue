fe16a3f54823c0483b56fcde240e54ca38e30427
holeczpeter <holecz.peter85@gmail.com>
2024-03-12 18:21:55 +0100

    add second google map

--- src/components/contact/ContactInfo.vue
+++ src/components/contact/ContactInfo.vue
@@ -2,17 +2,7 @@
    <section class="ttm-row conatact-section clearfix">
       <div class="container">
          <div class="row">
-            <div class="col-lg-5">
-               <iframe  title="location" 
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2724.3270895791907!2d20.0715225760728!3d46.93561013407185!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x474158a035b0b365%3A0xb29078aae4ed0bbe!2sTiszak%C3%A9cske%2C%20D%C3%B3zsa%20Telep%20u.%2083%2C%206060!5e0!3m2!1shu!2shu!4v1705164223073!5m2!1shu!2shu" 
-                        width="600" 
-                        height="550"  
-                        allowfullscreen="" 
-                        loading="lazy" 
-                        referrerpolicy="no-referrer-when-downgrade">
-               </iframe>
-            </div>
-            <div class="col-lg-7">
+            <div class="col">
                <div class="padding_top20 res-991-padding_top40">
                   <div class="section-title">
                      <div class="title-header">
@@ -26,7 +16,17 @@
                   <h2 class="fs-24 padding_top10">{{ $t('contact-us-page.head-office')}}</h2>
                   <div class="ttm-horizontal_sep width-100"></div>
                   <div class="row">
-                     <div class="col-lg-6 col-md-6">
+                   <div class="col-lg-6 margin_top15">
+                        <iframe  title="location" 
+                                 src="https://www.google.com/maps/embed/v1/place?q=De+Potterestraat+1b,+Oud+Gastel,+Hollandia&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8" 
+                                 width="600" 
+                                 height="250"  
+                                 allowfullscreen="" 
+                                 loading="lazy" 
+                                 referrerpolicy="no-referrer-when-downgrade">
+                        </iframe>
+                     </div>
+                     <div class="col-lg-3 col-md-6">
                         <div class="featured-icon-box icon-align-before-content">
                            <div class="featured-icon">
                               <div class="ttm-icon ttm-icon_element-fill ttm-icon_element-color-grey ttm-icon_element-size-md ttm-icon_element-style-rounded"> 
@@ -41,7 +41,7 @@
                            </div>
                         </div>
                      </div>
-                     <div class="col-lg-6 col-md-6">
+                     <div class="col-lg-3 col-md-6">
                         <div class="featured-icon-box icon-align-before-content">
                            <div class="featured-icon">
                               <div class="ttm-icon ttm-icon_element-fill ttm-icon_element-color-grey ttm-icon_element-size-md ttm-icon_element-style-rounded"> 
@@ -60,7 +60,17 @@
                   <h2 class="fs-24 padding_top30">{{ $t('contact-us-page.branch-office')}}</h2>
                   <div class="ttm-horizontal_sep width-100"></div>
                   <div class="row">
-                     <div class="col-lg-6 col-md-6">
+                     <div class="col-lg-6 margin_top15">
+                        <iframe  title="location" 
+                                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2724.3270895791907!2d20.0715225760728!3d46.93561013407185!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x474158a035b0b365%3A0xb29078aae4ed0bbe!2sTiszak%C3%A9cske%2C%20D%C3%B3zsa%20Telep%20u.%2083%2C%206060!5e0!3m2!1shu!2shu!4v1705164223073!5m2!1shu!2shu" 
+                                 width="600" 
+                                 height="250"  
+                                 allowfullscreen="" 
+                                 loading="lazy" 
+                                 referrerpolicy="no-referrer-when-downgrade">
+                        </iframe>
+                     </div>
+                     <div class="col-lg-3 col-md-6">
                         <div class="featured-icon-box icon-align-before-content">
                            <div class="featured-icon">
                               <div class="ttm-icon ttm-icon_element-fill ttm-icon_element-color-grey ttm-icon_element-size-md ttm-icon_element-style-rounded"> 
@@ -75,7 +85,7 @@
                            </div>
                         </div>
                      </div>
-                     <div class="col-lg-6 col-md-6">
+                     <div class="col-lg-3 col-md-6">
                         <!--featured-icon-box-->
                         <div class="featured-icon-box icon-align-before-content">
                            <div class="featured-icon">
